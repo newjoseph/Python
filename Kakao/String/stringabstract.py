d1718da8ec3e3b61724d054eca28c67a2df2e77d
Minhyuk Kim <43889699+newjoseph@users.noreply.github.com>
2021-01-19 16:21:23 +0900

    commit2

--- Kakao/String/stringabstract.py
+++ Kakao/String/stringabstract.py
@@ -117,6 +117,9 @@ def solution(s):
     # how many substring is in the string
     counter = 1
 
+    #flag for checking the last input or not
+    flag = false
+
     # for loop for the length of strings from 1 to 1000
     for i in range(1,int(len(s)/2) + 1):
 
@@ -124,43 +127,59 @@ def solution(s):
       this_str = s[0:i]
 
       #test
-      print( "i is " + "{}".format(i) + " string is "+ this_str)
+      print( "length of string is " + "{}".format(i) + " string is "+ this_str +"\n")
 
       for x in range(len(this_str), len(s), len(this_str)):
 
   #print("{}".format(x)+"th trial" )
         substring = s[x:x + len(this_str)]
   #print("{}".format(x)+"th trial after" )
-        print("substring is: " + substring)
+        print("current string is: " + substring)
         
         # substring matches
         if this_str == substring:
-
-          if x != len(s):
-            counter += 1  
+          print("string matches! ") #debug
+          counter += 1 
+          # if this is not the last character
+          if x + len(this_str) != len(s):
+          #if x != len(s):
+            #print("continue! " + "{}".format(x) + " "+"{}".format(len(s)))
             continue
+          # Last char case.
           else:             
             # update the temp_str
-            temp_str = temp_str + "{}".format(counter)+ string
+            #temp_str = temp_str + "{}".format(counter)+ string + "{}".format(x) 
+            temp_str = temp_str + "{}".format(counter)+ substring #+ "{}".format(x) 
 
         # substring does not match
+               
         else:
+          print("string does not matches! ")   # debug
+          print("before temp_str: " + temp_str )
           # if there are more than one matching
           if counter != 1:
             # update the temp_str
-            temp_str = temp_str + "{}".format(counter)+ this_str
+            #temp_str = temp_str + "{}".format(counter)+ this_str
+            temp_str = temp_str + "{}".format(counter) + this_str
           # just one string 
           else:
             temp_str += this_str
+          print("after temp_str: " + temp_str)
+
+
+
+
         
+        print("case "+ "i: " "{}".format(i) + " x: " + "{}".format(x) +" temp_string: " + temp_str + " this_str: " + this_str + " temp_str: " + temp_str + " substring:"+ substring +" counter: " + "{}".format(counter))
+        #print("temp_str: " + temp_str )
         # change the working string
         this_str = substring
         # reset the counter
         counter = 1
 
-        print("nested :" + "{}".format(x) +" temp_string: " + temp_str + " this_str: " + this_str)
+  #      print("Loop2 :" + "{}".format(x) +" temp_string: " + temp_str + " this_str: " + this_str)
 
-      print("for loop :" + "{}".format(x) +" temp_string: " + temp_str + " this_str: " + this_str)
+ #     print("Loop1 :" + "{}".format(x) +" temp_string: " + temp_str + " this_str: " + this_str)
 
       #check the min length
       if answer > len(temp_str):
