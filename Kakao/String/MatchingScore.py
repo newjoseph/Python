6ce69023dd2cdbde6222b53327a61883e1a535c9
Minhyuk Kim <43889699+newjoseph@users.noreply.github.com>
2021-02-22 23:21:27 +0900

    Update MatchingScore.py

--- Kakao/String/MatchingScore.py
+++ Kakao/String/MatchingScore.py
@@ -1,5 +1,8 @@
 
-# need to add a case if there are more than one link
+# need to add a link score
+
+# 2019 kakao recruting #6
+
 
 # 2019 kakao recruting #6
 
@@ -7,14 +10,15 @@ def solution(word, pages):
 
   url, body, link = "", "", ""
   n_link, score = 0
-  first, last = 0, 0
+  first, last, cnt = 0, 0, 0
   word = word.lower()
-
+  dic, linkDic = {} , {}
 
   for x in pages:
     print("----------------")
     #print(x)
 
+    #find url
     first = x.find("content=\"https:") + 17
     print("index: " + "{}".format(first))
     second = x.find("\"/>",first)
@@ -26,23 +30,58 @@ def solution(word, pages):
     second = x.find("</body>",first)
     body = x[first:second]
     print("BODY: " + body)
-    
-    
-
 
     second = body.find("<a href=\"https://")
     body_content = body[:second]
     print(body_content)
 
-    first = second + 17
-    print("index: " + "{}".format(first))
-    second = body.find("\">",first)
-    link = body[first:second].strip()
-    print("LINK: " + link)
+    cnt = x.count("<a href=\"https://")
+    for i in range(cnt):
+      first = body.find("<a href=\"https://") + 17
+      second = second = body.find("\">",first)
+      link = body[first:second].strip()
+      linkDic[i] = link
+    
+    
+    dic[url] = linkDic
+    
+    #find the basic score
+    new_cont = body_content.lower()
+    while:
+      first = new_cont.find(word)
+      if first != -1:
+        break
+      else:
+        last = first + len(word)
+        if !new_cont[last].isalpha():
+          score += score
+
+    
+
+      
+
+
+
+
+     
+
+  
     
 
 
   
   answer = 0
   return answer
+
+
+str1 = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+word = ""
+
+string1 = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+string2 = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]
+
+
+solution(word, str1)
+
+
 
